f5fbc7f618d62bbf82fc4a8da591d5ffa05c0bfd
Dinghao Pan <dinghaopan@mail.dlut.edu.cn>
2023-10-25 09:31:45 +0800

    fix chat bug

--- chat/single_chat.py
+++ chat/single_chat.py
@@ -13,14 +13,14 @@ from utils import ModelUtils
 
 # 使用合并后的模型进行推理
 model_name_or_path = "Qwen_model/Qwen/Qwen-7B"      # Qwen模型权重路径
-adapter_name_or_path = "output_qwen/checkpoint-10125"     # sft后adapter权重路径
+adapter_name_or_path = "output_qwen/checkpoint-1125"     # sft后adapter权重路径
 
 # 使用base model和adapter进行推理，无需手动合并权重
 # model_name_or_path = 'baichuan-inc/Baichuan-7B'
 # adapter_name_or_path = 'YeungNLP/firefly-baichuan-7b-qlora-sft'
 
 # 是否使用4bit进行推理，能够节省很多显存，但效果可能会有一定的下降
-load_in_4bit = False
+load_in_4bit = True
 # 生成超参配置
 max_new_tokens = 500
 top_p = 0.9
@@ -48,8 +48,9 @@ def main():
     while True:
         conversation = [{"from": "user", "value": text}]
         print("conversation" + str(conversation))
+        im_start = tokenizer.im_start_id
         tokenizer.pad_token_id = tokenizer.eod_id
-        data_dict = preprocess([conversation], tokenizer, 1024)
+        data_dict = preprocess([conversation], tokenizer, 1024, test_flag = True)
         print(data_dict)
         input_ids = data_dict["input_ids"].to(device)
         labels = data_dict["labels"]
